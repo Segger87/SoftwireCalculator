56650cb012181bb9629e926311a7dfae847f7d7e
Segger87 <Segger87@gmail.com>
2018-03-27 17:05:26 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Calculator");
+            Console.WriteLine("Enter one number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            int result = firstNumber * secondNumber;
+            Console.WriteLine(firstNumber + " X " + secondNumber + " = " + result);
+            Console.ReadLine();
+        }
+    }
+}
