3060098aecb0c75634dd812e44aa75719d3f1b70
Segger87 <Segger87@gmail.com>
2018-03-28 23:30:12 +0100

    Added Array looping for inputs

--- Program.cs
+++ Program.cs
@@ -9,18 +9,46 @@ namespace Calculator
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Welcome to the Calculator");
-            Console.WriteLine("Enter one number: ");
 
-            int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter a second number: ");
+            Console.WriteLine("Please enter the mathmatical operator: ");
+            string op = Console.ReadLine();
 
-            int secondNumber = int.Parse(Console.ReadLine());
-            int result = firstNumber * secondNumber;
+            Console.WriteLine("Please enter how many numbers you wish to calculate: ");
+            int numberOfValues = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(firstNumber + " X " + secondNumber + " = " + result);
-            Console.ReadLine();
+            int[] numbersInput = new int[numberOfValues];
+
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                Console.Write("Please enter number " + (i + 1) + ": ");
+                numbersInput[i] = int.Parse(Console.ReadLine());
+            }
+
+            int result = numbersInput[0];
+
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                if (op == "+")
+                {
+                    result = result + numbersInput[i];
+                }
+                else if (op == "-")
+                {
+                    result = result - numbersInput[i];
+                }
+                else if (op == "*")
+                {
+                    result = result * numbersInput[i];
+                }
+                else if (op == "/")
+                {
+                    result = result / numbersInput[i];
+                }
+            }
+                Console.WriteLine("The answer is: " + result);
+                Console.ReadLine();
         }
     }
 }
