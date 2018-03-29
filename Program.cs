b609e037289312c37422fcd11872e46f7a19ae88
Segger87 <Segger87@gmail.com>
2018-03-29 11:35:05 +0100

    Add Methods and TryParse to prevent wrong input

--- Program.cs
+++ Program.cs
@@ -9,17 +9,28 @@ namespace Calculator
     class Program
     {
         static void Main(string[] args)
-        { 
-            Console.WriteLine("Welcome to the Calculator");
+        {
+            while(true)
+            {
+                PrintWelcomeMessage();
+                string op = OperatorName();
+                int numberOfValues = NumberOfCalculations();
+                int i = PerformOneCalculation(op, numberOfValues);
 
-            Console.WriteLine("Please enter the mathmatical operator: ");
-            string op = Console.ReadLine();
+                Console.ReadLine();
+            }
+        }
 
-            Console.WriteLine("Please enter how many numbers you wish to calculate: ");
-            int numberOfValues = int.Parse(Console.ReadLine());
+        private static void PrintWelcomeMessage()
+            {
+                Console.WriteLine("Welcome to the Calculator");
+            }
 
+        private static int PerformOneCalculation(string op, int numberOfValues)
+            {
+        
             int[] numbersInput = new int[numberOfValues];
-
+         
             for (int i = 0; i < numberOfValues; i++)
             {
                 Console.Write("Please enter number " + (i + 1) + ": ");
@@ -28,7 +39,7 @@ namespace Calculator
 
             int result = numbersInput[0];
 
-            for (int i = 0; i < numberOfValues; i++)
+            for (int i = 1; i < numbersInput.Length; i++)
             {
                 if (op == "+")
                 {
@@ -47,8 +58,26 @@ namespace Calculator
                     result = result / numbersInput[i];
                 }
             }
-                Console.WriteLine("The answer is: " + result);
-                Console.ReadLine();
+            Console.WriteLine("The answer is: " + result);
+            return result;
+        }
+        private static string OperatorName()
+        {
+            Console.WriteLine("Please enter the mathmatical operator: ");
+            string op = Console.ReadLine();
+            return op;
+        }
+
+        private static int NumberOfCalculations()
+        {
+            int numberOfValues = 0;
+
+        do
+        {
+            Console.WriteLine("Please enter how many numbers you wish to calculate: ");
+        } while (!int.TryParse(Console.ReadLine(), out numberOfValues));
+
+           return numberOfValues;
         }
     }
 }
